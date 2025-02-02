3640609dd66d93657eb586160d97c5b42b375c36
AKIRA.404 <akira.404@foxmail.com>
2025-02-02 15:21:44 +0800

    main.lua

--- main.lua
+++ main.lua
@@ -1,46 +1,43 @@
 local Rect = require("src/Rect")
 local Text = require("src/text")
 local Spring = require("src/spring")
+local Bounce = require("src/bounce")
 local utils = require("src/utils")
 local color = require("src/color")
 
 function love.load()
+    love.graphics.setBackgroundColor(color.deep_gray)
     local win_cx, win_cy = utils.get_windows_cxcy()
 
     local font_path = "assets/fonts/FatPixelFont.ttf"
 
     open_btn = Rect:new("fill", color.red)
     open_btn_text = Text:new("OPEN", 20, color.white, font_path)
-    open_btn_spring = Spring:new()
-    open_btn_spring:init(300, 100, 10)
 
-    open_btn:set_cxcywh(win_cx, win_cy, 300, 100, 20, 20, 20)
+    open_btn:set_cxcywh(win_cx, win_cy, 300, 100)
+    open_btn:set_other_attributes(20, 20, 20)
+
     open_btn_text:set_cxcy(win_cx, win_cy + 20)
+    open_btn_w_spring = Spring:new(open_btn.w, 50, 5)
+    open_btn_h_spring = Spring:new(open_btn.h, 50, 5)
+    open_btn_text_bounce = Bounce:new(2, 5)
 end
 
 function love.update(dt)
-    open_btn_spring:update(dt)
-    open_btn.w = open_btn_spring.x
-    open_btn.x = open_btn.cx - open_btn.w / 2
-    -- open_btn.h = open_btn_spring.x
+    open_btn_text_bounce:sin_bounce_update(dt)
+    open_btn_w_spring:update(dt)
+    open_btn_h_spring:update(dt)
 end
 
 function love.draw()
     open_btn:draw()
-    open_btn_text:draw()
+    open_btn_text:draw(nil, open_btn_text.y + open_btn_text_bounce.offset)
 end
 
 function love.mousemoved(mx, my)
-    if utils.is_mouse_in_rect(mx, my, { open_btn:get_xywh() }) then
-        open_btn.color = color.blud
-    else
-        open_btn.color = color.red
-    end
 end
 
 function love.mousepressed(x, y, button)
     if utils.is_mouse_in_rect(x, y, { open_btn:get_xywh() }) then
-        open_btn_spring:pull(50)
-        open_btn:translate(open_btn.cx, open_btn.cy)
     end
 end
